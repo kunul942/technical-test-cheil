7307fc4dd2374696b342d8b2bb33ae9da3a1bed1
leek117749 <kunu.lr@alliedglobal.com>
2025-04-18 16:47:41 -0600

    feat(controller): create basic crud in controller

--- server/Program.cs
+++ server/Program.cs
@@ -4,9 +4,10 @@ using Server.database;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers(); // This line is CRUCIAL for API controllers
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseInMemoryDatabase("UserDB"));
 
@@ -21,29 +22,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+// This line is ESSENTIAL to map your controller routes
+app.MapControllers(); 
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
--- server/database/AppDbContext.cs
+++ server/database/AppDbContext.cs
@@ -6,7 +6,6 @@ namespace Server.database
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-        public DbSet<User> Userws { get; set; }
-        public object Users { get; internal set; }
+        public DbSet<User> Users { get; set; }
     }
 }
