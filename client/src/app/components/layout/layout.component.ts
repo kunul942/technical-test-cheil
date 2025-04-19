ffad7b0c94ae5f48cf6d361e94f3e6e0ceb9331e
leek117749 <kunu.lr@alliedglobal.com>
2025-04-19 13:11:01 -0600

    feat(client): layout component

--- client/src/app/components/layout/layout.component.ts
+++ client/src/app/components/layout/layout.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
   templateUrl: './layout.component.html',
-  styleUrl: './layout.component.scss'
+  styleUrls: ['./layout.component.scss']
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit {
+  isAdmin = false;
 
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.isAdmin = this.authService.isAdmin;
+  }
+
+  logout(): void {
+    this.authService.logout();
+  }
 }
