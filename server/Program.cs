24fdf486d1303bbce40ccd39c0e11dd509fbec08
leek117749 <kunu.lr@alliedglobal.com>
2025-04-19 13:55:42 -0600

    feat(cors): allow cors for web app and all domains

--- server/Program.cs
+++ server/Program.cs
@@ -13,6 +13,17 @@ using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//  Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 // Add Controllers + FluentValidation
 builder.Services.AddControllers();
 builder.Services.AddFluentValidationAutoValidation()
@@ -120,6 +131,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
+
 app.UseAuthentication();
 app.UseAuthorization();
 
