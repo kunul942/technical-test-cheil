b2d2159e9d71269ae9e69698b918eace2212dc18
leek117749 <kunu.lr@alliedglobal.com>
2025-04-19 22:53:49 -0600

    feat: read me documentation

--- server/models/User.cs
+++ server/models/User.cs
@@ -7,8 +7,8 @@ namespace Server.Models
         public required string FirstName { get; set; }
         public required string LastName { get; set; }
         public required string Email { get; set; }
-        public required string Password { get; set; }  // Will hash later
-        public required string Role { get; set; }      // "Admin" or "User"
+        public required string Password { get; set; }
+        public required string Role { get; set; }
         
     }
 }
